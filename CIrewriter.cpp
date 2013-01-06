16ee9b6e635db78aa38671781c434fd0d6174489
Larry Olson <loarabia@hotmail.com>
2013-01-06 12:53:36 -0800

    Merging in RRankene's latest changes.

--- CIrewriter.cpp
+++ CIrewriter.cpp
@@ -40,17 +40,20 @@
  * Interesting information is printed on stderr.
  *
  * Usage:
- * CIrewriter <file>.c
+ * CIrewriter <options> <file>.c
+ * where <options> allow for parameters to be passed to the preprocessor
+ * such as -DFOO to define FOO.
  *
  * Generated as output <file>_out.c
  *
  * Note: This tutorial uses the CompilerInstance object which has as one of
- * its purpose to create commonly used Clang types.
+ * its purposes to create commonly used Clang types.
  *****************************************************************************/
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdio.h>
+#include <vector>
 
 #include "llvm/Support/Host.h"
 #include "llvm/Support/raw_ostream.h"
@@ -80,13 +83,13 @@ class MyRecursiveASTVisitor
 {
 
  public:
+  MyRecursiveASTVisitor(Rewriter &R) : Rewrite(R) { }
   void InstrumentStmt(Stmt *s);
   bool VisitStmt(Stmt *s);
   bool VisitFunctionDecl(FunctionDecl *f);
   Expr *VisitBinaryOperator(BinaryOperator *op);
 
-  Rewriter Rewrite;
-  CompilerInstance *ci;
+  Rewriter &Rewrite;
 };
 
 // Override Binary Operator expressions
@@ -95,16 +98,16 @@ Expr *MyRecursiveASTVisitor::VisitBinaryOperator(BinaryOperator *E)
   // Determine type of binary operator
   if (E->isLogicalOp())
   {
-    // Replace operator ("||" or "&&") with ","
-    Rewrite.ReplaceText(E->getOperatorLoc(), E->getOpcodeStr().size(), ",");
-
     // Insert function call at start of first expression.
     // Note getLocStart() should work as well as getExprLoc()
     Rewrite.InsertText(E->getLHS()->getExprLoc(),
              E->getOpcode() == BO_LAnd ? "L_AND(" : "L_OR(", true);
 
+    // Replace operator ("||" or "&&") with ","
+    Rewrite.ReplaceText(E->getOperatorLoc(), E->getOpcodeStr().size(), ",");
+
     // Insert closing paren at end of right-hand expression
-    Rewrite.InsertText(E->getRHS()->getLocEnd(), ")", true);
+    Rewrite.InsertTextAfterToken(E->getRHS()->getLocEnd(), ")");
   }
   else
   // Note isComparisonOp() is like isRelationalOp() but includes == and !=
@@ -148,8 +151,8 @@ void MyRecursiveASTVisitor::InstrumentStmt(Stmt *s)
     // MeasureTokenLength gets us past the last token, and adding 1 gets
     // us past the ';'.
     int offset = Lexer::MeasureTokenLength(END,
-                                           ci->getSourceManager(),
-                                           ci->getLangOpts()) + 1;
+                                           Rewrite.getSourceMgr(),
+                                           Rewrite.getLangOpts()) + 1;
 
     SourceLocation END1 = END.getLocWithOffset(offset);
     Rewrite.InsertText(END1, "\n}", true, true);
@@ -247,111 +250,13 @@ bool MyRecursiveASTVisitor::VisitFunctionDecl(FunctionDecl *f)
 class MyASTConsumer : public ASTConsumer
 {
  public:
-    MyASTConsumer(const char *f);
-    virtual bool HandleTopLevelDecl(DeclGroupRef d);
 
-    MyRecursiveASTVisitor rv;
+  MyASTConsumer(Rewriter &Rewrite) : rv(Rewrite) { }
+  virtual bool HandleTopLevelDecl(DeclGroupRef d);
+
+  MyRecursiveASTVisitor rv;
 };
 
-
-MyASTConsumer::MyASTConsumer(const char *f)
-{
-  rv.ci = new CompilerInstance();
-  rv.ci->createDiagnostics(0,NULL);
-
-  llvm::IntrusiveRefCntPtr<TargetOptions> pto( new TargetOptions());
-  pto->Triple = llvm::sys::getDefaultTargetTriple();
-  llvm::IntrusiveRefCntPtr<TargetInfo> pti(TargetInfo::CreateTargetInfo(rv.ci->getDiagnostics(), pto.getPtr()));
-  rv.ci->setTarget(pti.getPtr());
-
-  rv.ci->createFileManager();
-  rv.ci->createSourceManager(rv.ci->getFileManager());
-
-  HeaderSearchOptions &headerSearchOptions = rv.ci->getHeaderSearchOpts();
-  // <Warning!!> -- Platform Specific Code lives here
-  // This depends on A) that you're running linux and
-  // B) that you have the same GCC LIBs installed that
-  // I do.
-  // Search through Clang itself for something like this,
-  // go on, you won't find it. The reason why is Clang
-  // has its own versions of std* which are installed under
-  // /usr/local/lib/clang/<version>/include/
-  // See somewhere around Driver.cpp:77 to see Clang adding
-  // its version of the headers to its include path.
-  headerSearchOptions.AddPath("/usr/include/linux",
-          clang::frontend::Angled,
-          false,
-          false,
-          false);
-  headerSearchOptions.AddPath("/usr/include/c++/4.4/tr1",
-          clang::frontend::Angled,
-          false,
-          false,
-          false);
-  headerSearchOptions.AddPath("/usr/include/c++/4.4",
-          clang::frontend::Angled,
-          false,
-          false,
-          false);
-  // </Warning!!> -- End of Platform Specific Code
-
-
-  // Allow C++ code to get rewritten
-  rv.ci->getLangOpts().GNUMode = 1; 
-  rv.ci->getLangOpts().CXXExceptions = 1; 
-  rv.ci->getLangOpts().RTTI = 1; 
-  rv.ci->getLangOpts().Bool = 1; 
-  rv.ci->getLangOpts().CPlusPlus = 1; 
-
-  rv.ci->createPreprocessor();
-  rv.ci->getPreprocessorOpts().UsePredefines = false;
-
-  rv.ci->setASTConsumer(this);
-
-  rv.ci->createASTContext();
-
-  // Initialize rewriter
-  rv.Rewrite.setSourceMgr(rv.ci->getSourceManager(), rv.ci->getLangOpts());
-
-  const FileEntry *pFile = rv.ci->getFileManager().getFile(f);
-  rv.ci->getSourceManager().createMainFileID(pFile);
-  rv.ci->getDiagnosticClient().BeginSourceFile(rv.ci->getLangOpts(),
-                                           &rv.ci->getPreprocessor());
-
-  // Convert <file>.c to <file_out>.c
-  std::string outName (f);
-  size_t ext = outName.rfind(".");
-  if (ext == std::string::npos)
-     ext = outName.length();
-  outName.insert(ext, "_out");
-
-  llvm::errs() << "Output to: " << outName << "\n";
-  std::string OutErrorInfo;
-  llvm::raw_fd_ostream outFile(outName.c_str(), OutErrorInfo, 0);
-
-  if (OutErrorInfo.empty())
-  {
-    // Parse the AST
-    ParseAST(rv.ci->getPreprocessor(), this, rv.ci->getASTContext());
-    rv.ci->getDiagnosticClient().EndSourceFile();
-
-    // Output some #ifdefs
-    outFile << "#define L_AND(a, b) a && b\n";
-    outFile << "#define L_OR(a, b) a || b\n\n";
-
-    // Now output rewritten source code
-    const RewriteBuffer *RewriteBuf =
-      rv.Rewrite.getRewriteBufferFor(rv.ci->getSourceManager().getMainFileID());
-    outFile << std::string(RewriteBuf->begin(), RewriteBuf->end());
-  }
-  else
-  {
-    llvm::errs() << "Cannot open " << outName << " for writing\n";
-  }
-
-  outFile.close();
-  delete rv.ci;
-}
 
 bool MyASTConsumer::HandleTopLevelDecl(DeclGroupRef d)
 {
@@ -366,24 +271,158 @@ bool MyASTConsumer::HandleTopLevelDecl(DeclGroupRef d)
 }
 
 
-
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
   struct stat sb;
 
-  if (argc != 2)
+  if (argc < 2)
   {
-     llvm::errs() << "Usage: CIrewriter <filename>\n";
+     llvm::errs() << "Usage: CIrewriter <options> <filename>\n";
      return 1;
   }
 
-  if (stat(argv[1], &sb) == -1)
+  // Get filename
+  std::string fileName(argv[argc - 1]);
+
+  // Make sure it exists
+  if (stat(fileName.c_str(), &sb) == -1)
   {
-    perror(argv[1]);
+    perror(fileName.c_str());
     exit(EXIT_FAILURE);
   }
 
-  MyASTConsumer *astConsumer = new MyASTConsumer(argv[1]);
+  CompilerInstance compiler;
+  compiler.createDiagnostics(argc, argv);
+
+  // Create an invocation that passes any flags to preprocessor
+  CompilerInvocation *Invocation = new CompilerInvocation;
+  CompilerInvocation::CreateFromArgs(*Invocation, argv + 1, argv + argc,
+                                      compiler.getDiagnostics());
+  compiler.setInvocation(Invocation);
+
+  // Set default target triple
+  llvm::IntrusiveRefCntPtr<TargetOptions> pto( new TargetOptions());
+  pto->Triple = llvm::sys::getDefaultTargetTriple();
+  llvm::IntrusiveRefCntPtr<TargetInfo>
+     pti(TargetInfo::CreateTargetInfo(compiler.getDiagnostics(),
+                                      pto.getPtr()));
+  compiler.setTarget(pti.getPtr());
+
+  compiler.createFileManager();
+  compiler.createSourceManager(compiler.getFileManager());
+
+  HeaderSearchOptions &headerSearchOptions = compiler.getHeaderSearchOpts();
+
+  // <Warning!!> -- Platform Specific Code lives here
+  // This depends on A) that you're running linux and
+  // B) that you have the same GCC LIBs installed that
+  // I do.
+  // Search through Clang itself for something like this,
+  // go on, you won't find it. The reason why is Clang
+  // has its own versions of std* which are installed under
+  // /usr/local/lib/clang/<version>/include/
+  // See somewhere around Driver.cpp:77 to see Clang adding
+  // its version of the headers to its include path.
+  // To see what include paths need to be here, try
+  // clang -v -c test.c
+  // or clang++ for C++ paths as used below:
+  headerSearchOptions.AddPath("/usr/include/c++/4.6",
+          clang::frontend::Angled,
+          false,
+          false,
+          false);
+  headerSearchOptions.AddPath("/usr/include/c++/4.6/i686-linux-gnu",
+          clang::frontend::Angled,
+          false,
+          false,
+          false);
+  headerSearchOptions.AddPath("/usr/include/c++/4.6/backward",
+          clang::frontend::Angled,
+          false,
+          false,
+          false);
+  headerSearchOptions.AddPath("/usr/local/include",
+          clang::frontend::Angled,
+          false,
+          false,
+          false);
+  headerSearchOptions.AddPath("/usr/local/lib/clang/3.3/include",
+          clang::frontend::Angled,
+          false,
+          false,
+          false);
+  headerSearchOptions.AddPath("/usr/include/i386-linux-gnu",
+          clang::frontend::Angled,
+          false,
+          false,
+          false);
+  headerSearchOptions.AddPath("/usr/include",
+          clang::frontend::Angled,
+          false,
+          false,
+          false);
+  // </Warning!!> -- End of Platform Specific Code
+
+
+  // Allow C++ code to get rewritten
+  LangOptions langOpts;
+  langOpts.GNUMode = 1; 
+  langOpts.CXXExceptions = 1; 
+  langOpts.RTTI = 1; 
+  langOpts.Bool = 1; 
+  langOpts.CPlusPlus = 1; 
+  Invocation->setLangDefaults(langOpts,
+                              clang::IK_CXX,
+                              clang::LangStandard::lang_cxx0x);
+
+  compiler.createPreprocessor();
+  compiler.getPreprocessorOpts().UsePredefines = false;
+
+  compiler.createASTContext();
+
+  // Initialize rewriter
+  Rewriter Rewrite;
+  Rewrite.setSourceMgr(compiler.getSourceManager(), compiler.getLangOpts());
+
+  const FileEntry *pFile = compiler.getFileManager().getFile(fileName);
+  compiler.getSourceManager().createMainFileID(pFile);
+  compiler.getDiagnosticClient().BeginSourceFile(compiler.getLangOpts(),
+                                                &compiler.getPreprocessor());
+
+  MyASTConsumer astConsumer(Rewrite);
+
+  // Convert <file>.c to <file_out>.c
+  std::string outName (fileName);
+  size_t ext = outName.rfind(".");
+  if (ext == std::string::npos)
+     ext = outName.length();
+  outName.insert(ext, "_out");
+
+  llvm::errs() << "Output to: " << outName << "\n";
+  std::string OutErrorInfo;
+  llvm::raw_fd_ostream outFile(outName.c_str(), OutErrorInfo, 0);
+
+  if (OutErrorInfo.empty())
+  {
+    // Parse the AST
+    ParseAST(compiler.getPreprocessor(), &astConsumer, compiler.getASTContext());
+    compiler.getDiagnosticClient().EndSourceFile();
+
+    // Output some #ifdefs
+    outFile << "#define L_AND(a, b) a && b\n";
+    outFile << "#define L_OR(a, b) a || b\n\n";
+
+    // Now output rewritten source code
+    const RewriteBuffer *RewriteBuf =
+      Rewrite.getRewriteBufferFor(compiler.getSourceManager().getMainFileID());
+    outFile << std::string(RewriteBuf->begin(), RewriteBuf->end());
+  }
+  else
+  {
+    llvm::errs() << "Cannot open " << outName << " for writing\n";
+  }
+
+  outFile.close();
 
   return 0;
 }
